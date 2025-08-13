39e25cf9cb30fff362324aada6a9209d3c610cb6
Rafael <rafael@iswe.co.nz>
2025-08-13 23:25:30 +1200

    Add initial implementation of RunarSEHost application with UI and entitlements
    This commit introduces the RunarSEHost application, including the main app structure, a basic user interface for generating or loading Secure Enclave keys, and the necessary entitlements for keychain access. Additionally, it sets up the project structure with asset catalogs for app icons and accent colors, and includes unit and UI tests to ensure functionality. The project configuration is also established in the Xcode project file.

---
+++ swift-keys/Examples/SEHostAppMac/RunarSEHost/RunarSEHost/RunarSEHost/RunarSEHostApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunarSEHostApp.swift
+//  RunarSEHost
+//
+//  Created by Rafael Almeida on 13/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct RunarSEHostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift-keys/Examples/SEHostAppMac/RunarSEHost/RunarSEHost/RunarSEHostTests/RunarSEHostTests.swift
@@ -0,0 +1,17 @@
+//
+//  RunarSEHostTests.swift
+//  RunarSEHostTests
+//
+//  Created by Rafael Almeida on 13/08/2025.
+//
+
+import Testing
+@testable import RunarSEHost
+
+struct RunarSEHostTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
